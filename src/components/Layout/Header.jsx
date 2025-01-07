208e6147ea7a8d8f00616737c7d434bd219fd496
kimmido <icecream120102@gmail.com>
2025-01-07 09:59:23 +0900

    logo 파일 위치변경

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import logo from '/delta_logo1.png';
 
 const Header = () => {
     return ( 
         <>
-            <h1><img src="src/assets/images/delta_logo1.png" alt="logo" /></h1>
+            <h1><img src="/delta_logo1.png" alt="logo" /></h1>
+            <div>
+                <ul>
+                    <li><a href="">사업소개</a></li>
+                    <li><a href="">회사소개</a></li>
+                </ul>
+            </div>
         </>
     );
 }
--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -5,6 +5,7 @@ const MainPage = () => {
     return ( 
         <>
         <Header />
+        <strong>Recycling robot system & Automation</strong>
         <strong>델타봇은 로봇산업의 선두 주자로 발돋움해 나갈 것 입니다.</strong>
         </>
      );
