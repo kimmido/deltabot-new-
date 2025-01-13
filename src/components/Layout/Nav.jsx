f7d3ce0438f8eccd206f7d44b1223041eae146e6
kimmido <icecream120102@gmail.com>
2025-01-13 17:58:09 +0900

    nav.jsx pc, mobile 레이아웃 및 스타일 수정 중

--- src/components/Layout/Nav.jsx
+++ src/components/Layout/Nav.jsx
@@ -1,9 +1,5 @@
 import React, { useRef, useState } from "react";
-import {
-  MobileMenuItem,
-  MobileSubMenuItem,
-  MobileMenuDropdown,
-} from "./Nav.styles";
+import styles from "./Nav.module.scss";
 import iconHamburger from "../../assets/images/icon/icon_menu.svg";
 import iconClose from "../../assets/images/icon/icon_close.svg";
 import gsap from "gsap";
@@ -13,138 +9,80 @@ gsap.registerPlugin(useGSAP);
 const Nav = () => {
   const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
   const [activeDropdown, setActiveDropdown] = useState(null);
-  const MobileMenuWrap = useRef(null);
   const mobileMenu = useRef(null);
-  const { contextSafe } = useGSAP({ scope: MobileMenuWrap });
 
-  const handleMouseEnter = (menu) => {
-    if (window.innerWidth > 768) {
-      setActiveDropdown(menu);
-    }
-  };
+  // const handleMouseEnter = (menu) => {
+  //   if (window.innerWidth > 768) {
+  //     setActiveDropdown(menu);
+  //   }
+  // };
 
-  const handleMouseLeave = () => {
-    if (window.innerWidth > 768) {
-      setActiveDropdown(null);
-    }
-  };
+  // const handleMouseLeave = () => {
+  //   if (window.innerWidth > 768) {
+  //     setActiveDropdown(null);
+  //   }
+  // };
 
   const toggleMobileMenu = () => {
     setMobileMenuOpen(!isMobileMenuOpen);
 
-    contextSafe(() => {
-      gsap.to(mobileMenu.current, { xPercent: -100 });
-    });
-  };
-
-  const toggleDropdown = (menu) => {
-    setActiveDropdown(activeDropdown === menu ? null : menu);
+    if (isMobileMenuOpen) gsap.to(mobileMenu.current, { xPercent: 0 });
+    if (!isMobileMenuOpen) gsap.to(mobileMenu.current, { xPercent: -100 });
   };
 
   const menuTitle = ["사업소개", "회사소개"];
   const menuData = {
     subMenu: ["자동화", "재활용", "의료", "IT"],
-    subCategories: {
-      자동화: [
-        "델타로봇",
-        "스카라",
-        "협업로봇",
-        "코봇",
-        "머신비전",
-        "열화상카메라",
-      ],
-      재활용: ["재활용시스템", "분광카메라", "조명"],
-      의료: ["PCR장비", "UV장비"],
-      IT: [
-        "GPU서버",
-        "Workstations",
-        "Rack Mount Systems",
-        "Military Systems",
-        "Transportation Computers",
-      ],
-    },
   };
-  // useGSAP(
-  //   (context, contextSafe) => {
-  //     // ✅ safe, wrapped in contextSafe() function
-  //     const onClick = contextSafe(() => {
-  //       gsap.to(mobileMenu.current, { xPercent: -100 });
-  //     });
 
-  //     mobileMenu.current.addEventListener("click", onClick);
-
-  //     // 👍 we remove the event listener in the cleanup function below.
-  //     return () => {
-  //       // <-- cleanup
-  //       mobileMenu.current.removeEventListener("click", onClick);
-  //     };
-  //   },
-  //   { scope: gsapContainer }
-  // );
   return (
-    <nav className="gnb">
+    <nav className={styles.gnb}>
       {/* PC 메뉴 */}
-      <div className="gnb__pc-wrap">
-        <div
-          className="gnb__link"
-          onMouseEnter={() => handleMouseEnter(menuData.title)}
-          onMouseLeave={handleMouseLeave}
-        >
-          <h4 className="gnb__title">{menuTitle[0]}</h4>
+      <div className={styles.pc}>
+        <div className={styles.pc__item}>
+          <div className={styles.pc__link}>
+            <h4 className={styles.pc__link__title}>{menuTitle[0]}</h4>
+          </div>
+          {menuData.subMenu.length > 0 && (
+            <ul className={styles.pc__sub__list}>
+              {menuData.subMenu.map((sub, idx) => (
+                <li className={styles.pc__sub__item} key={sub}>
+                  <h5>{sub}</h5>
+                </li>
+              ))}
+            </ul>
+          )}
         </div>
-        {menuData.subMenu.length > 0 && (
-          <ul className="submenu">
-            {menuData.subMenu.map((sub, idx) => (
-              <li className="submenu__item" key={sub}>
-                <h5>{sub}</h5>
-                {menuData.subCategories[sub] && (
-                  <div className="submenu_linkbox">
-                    {menuData.subCategories[sub].map((item) => (
-                      <li className="submenu__link" key={item}>
-                        {item}
-                      </li>
-                    ))}
-                  </div>
-                )}
-              </li>
-            ))}
-          </ul>
-        )}
-        <div className="gnb__link">
-          <h4 className="gnb__title">{menuTitle[1]}</h4>
+        <div className={styles.pc__item}>
+          <div className={styles.pc__link}>
+            <h4 className={styles.pc__link__title}>{menuTitle[1]}</h4>
+          </div>
         </div>
       </div>
 
       {/* 모바일 메뉴 */}
-      <div className="gnb__mobile-wrap" ref={MobileMenuWrap}>
-        <button className="menu-icon" onClick={toggleMobileMenu}>
+      <div className={styles.mobile}>
+        <button className={styles.mobile__btn} onClick={toggleMobileMenu}>
           {isMobileMenuOpen ? (
             <img src={iconClose} alt="메뉴 닫기 버튼" />
           ) : (
             <img src={iconHamburger} alt="메뉴 열기 버튼" />
           )}
         </button>
-        <ul className="mobile-menu" ref={mobileMenu}>
-          <MobileMenuItem>
+        <ul className={styles.mobile__list} ref={mobileMenu}>
+          <li className={styles.mobile__item}>
             <h4>{menuTitle[0]}</h4>
-            <ul>
+            <ul className={styles.mobile__sub__list}>
               {menuData.subMenu.map((sub) => (
-                <MobileSubMenuItem key={sub}>
-                  <h5 onClick={() => toggleDropdown(sub)}>{sub}</h5>
-                  {menuData.subCategories[sub] && activeDropdown === sub && (
-                    <MobileMenuDropdown>
-                      {menuData.subCategories[sub].map((item) => (
-                        <li key={item}>{item}</li>
-                      ))}
-                    </MobileMenuDropdown>
-                  )}
-                </MobileSubMenuItem>
+                <li className={styles.mobile__sub__item} key={sub}>
+                  <h5>{sub}</h5>
+                </li>
               ))}
             </ul>
-          </MobileMenuItem>
-          <MobileMenuItem>
+          </li>
+          <li className={styles.mobile__item}>
             <h4>{menuTitle[1]}</h4>
-          </MobileMenuItem>
+          </li>
         </ul>
       </div>
     </nav>
--- src/components/Layout/Nav.styles.js
+++ src/components/Layout/Nav.styles.js
@@ -3,55 +3,6 @@ import iconArrow from "../../assets/images/icon/icon_keyboard_arrow_up.svg"
 import { respondTo } from "../../assets/styles/responsive";
 
 // ========= mobile =========
-export const MobileMenuWrap = styled.div`
-  display: block;
-
-  ${respondTo('large')} {
-    display: none;
-  }
-`;
-
-export const MenuIcon = styled.button`
-  position: relative;
-  width: 40px;
-  height: 40px;
-  margin-right: -8px;
-
-  z-index: 1000;
-`;
-
-export const MobileMenu = styled.ul`
-  position: absolute;
-  left: 100%;
-  top: -12px;
-  display: flex;
-  flex-direction: column;
-  width: 100vw;
-  height: 100vh;
-  background: white;
-  padding: 64px 16px 16px;
-  animation: ${({ isOpen }) => (isOpen ? slideIn : slideOut)} 0.3s forwards;
-
-  z-index: 900;
-`;
-
-const slideIn = keyframes`
-  from {
-    left: 100%;
-  }
-  to {
-    left: -16px;
-  }
-`;
-
-const slideOut = keyframes`
-  from {
-    left: -16px;
-  }
-  to {
-    left: 100%;
-  }
-`;
 
 export const MobileMenuItem = styled.li`
   margin: 10px 0;
@@ -61,15 +12,6 @@ export const MobileMenuItem = styled.li`
   }
 `;
 
-export const MobileMenuTitle = styled.h4`
-  font-size: 32px;
-`;
-
-export const MobileSubMenu = styled.ul`
-`;
-
-export const MobileSubMenuTitle = styled.h5`
-`;
 
 export const MobileSubMenuItem = styled.li`
 margin-bottom: 4px;
@@ -92,15 +34,5 @@ margin-bottom: 4px;
       height: 24px;
       background: no-repeat url("${iconArrow}");
     }
-  }
-`;
-
-export const MobileMenuDropdown = styled.ul`
-  margin: 0 15px;
-  padding: 15px 0;
-
-  li {
-    padding: 9px 22px;
-    font-size: 17px;
   }
 `;
