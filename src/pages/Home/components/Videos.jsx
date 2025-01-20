997ff2e2737ada089733772c45204ee0bb3de798
kimmido <icecream120102@gmail.com>
2025-01-20 17:57:46 +0900

    동영상 레이아웃 잡는 중

--- src/pages/Home/components/Videos.jsx
+++ src/pages/Home/components/Videos.jsx
@@ -4,9 +4,18 @@ function Videos() {
   return (
     <section className="Videos">
       <div className="container">
-        <video controls preload="none">
-          <source src="" />
-        </video>
+        <h2>Our Video</h2>
+        <div className="flex-box">
+          <video controls preload="none">
+            <source src="" />
+          </video>
+          <div>
+            <p>DELTABOT PROJECT VIDEOS</p>
+            <button>PET Positive Sort AMP Customer Facility Footage</button>
+            <button>PET Clear QC</button>
+            <button>델타로봇을 이용한 자동화라인</button>
+          </div>
+        </div>
       </div>
     </section>
   );
