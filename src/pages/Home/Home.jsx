d1f5537578fe3ffb8049c9451b468f66f0273c74
kimmido <icecream120102@gmail.com>
2025-02-03 17:48:02 +0900

    불필요한 코드 제거

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,35 +4,11 @@ import BusinessSection from "./components/BusinessSection";
 import Videos from "./components/Videos";
 
 const Home = () => {
-  // const gsapContainer = useRef(null);
-
-  // useGSAP(
-  //   () => {
-  //     gsap.to(".decorative-txt", {
-  //       scrollTrigger: {
-  //         trigger: ".decorative-txt",
-  //         start: "bottom 90%",
-  //         end: "center 30%",
-  //         scrub: 1, // 스크롤에 따라 부드럽게
-  //         markers: false,
-  //       },
-  //       backgroundSize: "100%",
-  //       ease: "none",
-  //     });
-  //   },
-  //   { scope: gsapContainer }
-  // );
-
   return (
     <div className="Home">
       <Slide />
-      {/* <div className="decorative-area" ref={gsapContainer}>
-          <p className="decorative-txt">델타봇은</p>
-          <p className="decorative-txt">로봇산업의 선두 주자로</p>
-          <p className="decorative-txt">발돋움해 나갈 것 입니다.</p>
-        </div> */}
       <BusinessSection />
-      {/* <Videos /> */}
+      <Videos />
     </div>
   );
 };
