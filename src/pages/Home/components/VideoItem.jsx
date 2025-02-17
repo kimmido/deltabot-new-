44f3cd31a55ced546d1ee58e7aea674c50d1a82a
kimmido <icecream120102@gmail.com>
2025-02-17 11:25:17 +0900

    비디오 섹션: 플레이 아이콘 추가, 모바일-pc 전환시 영상재생 멈춤

--- src/pages/Home/components/VideoItem.jsx
+++ src/pages/Home/components/VideoItem.jsx
@@ -25,13 +25,8 @@ function VideoItem({ title, id, desc }) {
   }, []);
 
   useEffect(() => {
-    if (isMobile) {
-      setIsExpanded(false);
-      videoBoxRef.current.style.width = "100%";
-      videoRef.current.pause();
-    } else {
-      videoBoxRef.current.style.width = "50%";
-    }
+    videoAction("pause");
+    videoBoxRef.current.style.width = isMobile ? "100%" : "50%";
   }, [isMobile]);
 
   useGSAP(() => {
@@ -51,39 +46,36 @@ function VideoItem({ title, id, desc }) {
           start: "top 80%",
           end: "center center",
           toggleActions: "play none none reverse",
-          //   onLeaveBack: handleClose,
         },
       }
     );
   });
 
-  const handleExpand = (e) => {
-    if (isExpanded || isMobile) return;
-
-    gsap.fromTo(
-      videoBoxRef.current,
-      {
-        width: "50%",
-      },
-      {
-        width: "90%",
-        delay: 0.06,
-        duration: 0.7,
-        onComplete: () => {
-          setIsExpanded(true);
-          setTimeout(() => {
-            videoRef.current.play();
-          }, 100);
+  const handlePlay = (isMobile, isExpanded) => {
+    if (isExpanded) return;
+    if (isMobile) {
+      videoAction("play");
+    } else {
+      gsap.fromTo(
+        videoBoxRef.current,
+        {
+          width: "50%",
         },
-      }
-    );
+        {
+          width: "90%",
+          delay: 0.06,
+          duration: 0.5,
+          onComplete: () => {
+            videoAction("play");
+          },
+        }
+      );
+    }
   };
 
   const handleClose = () => {
     if (isExpanded) {
-      setIsExpanded(false);
-      videoRef.current.pause();
-      videoRef.current.currentTime = 0;
+      videoAction("pause");
 
       gsap.to(videoBoxRef.current, {
         width: "50%",
@@ -92,35 +84,60 @@ function VideoItem({ title, id, desc }) {
     }
   };
 
+  function videoAction(action) {
+    if (action == "play") {
+      setIsExpanded(true);
+      setTimeout(() => {
+        videoRef.current.play();
+      }, 100);
+    }
+    if (action == "pause") {
+      setIsExpanded(false);
+      videoRef.current.pause();
+      videoRef.current.currentTime = 0;
+    }
+  }
+
   return (
     <div key={title} className={`VideoItem`} ref={containerRef}>
       <div
         className={`video-box ${isMobile ? "mobile" : "pc"}`}
         ref={videoBoxRef}
-        onClick={(e) => handleExpand(e)}
+        onClick={() => handlePlay(isMobile, isExpanded)}
       >
         <video
           preload="none"
           poster="/images/video_poster/poster1.jpg"
           playsInline
-          controls={isExpanded || isMobile}
+          controls={isExpanded}
           ref={videoRef}
         >
           <source src={`/videos/main/main${id}.mp4`} type="video/mp4" />
           브라우저가 비디오 태그를 지원하지 않습니다.
         </video>
+        {!isExpanded && (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 280 280"
+            xmlSpace="preserve"
+          >
+            <path
+              d="M183.028 43.028H96.972C43.501 43.028 0 86.529 0 140s43.501 96.972 96.972 96.972h86.057c53.47 0 96.971-43.501 96.971-96.972s-43.501-96.972-96.972-96.972m0 175.944H96.972C53.427 218.972 18 183.545 18 140s35.427-78.972 78.972-78.972h86.057C226.573 61.028 262 96.455 262 140s-35.427 78.972-78.972 78.972"
+              fill="#fff"
+            />
+            <path
+              d="m177.029 129.326-42.208-35.29c-9.058-7.573-22.838-1.133-22.838 10.674v70.58c0 11.807 13.78 18.248 22.838 10.674l42.208-35.29c6.652-5.561 6.652-15.787 0-21.348"
+              fill="#fff"
+            />
+          </svg>
+        )}
         {isExpanded && !isMobile && (
           <button ref={closeRef} className="close-btn" onClick={handleClose}>
             X
           </button>
         )}
       </div>
-      <div
-        className="text-box"
-        // data-aos="fade-up"
-        // data-aos-duration="1000"
-        // data-aos-anchor-placement="center-center"
-      >
+      <div className="text-box">
         <strong className="title">{title}</strong>
         <div className="desc">
           {desc.map((p, idx) => (
