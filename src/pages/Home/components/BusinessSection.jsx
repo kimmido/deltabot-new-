54160b1a8105cbea34c1dca42df787ccff1a5b76
kimmido <icecream120102@gmail.com>
2025-02-18 10:34:47 +0900

    대분류 링크 추가

--- src/pages/Home/components/BusinessSection.jsx
+++ src/pages/Home/components/BusinessSection.jsx
@@ -1,31 +1,34 @@
-import React, { useContext, useEffect, useRef } from "react";
+import React, { useContext } from "react";
 import styles from "./BusinessSection.module.scss";
 import { CategoryRoutesContext } from "../../../contexts/CategoryRoutesContext";
 import { Link } from "react-router-dom";
 
 function BusinessSection() {
   const category = useContext(CategoryRoutesContext);
-  const routes = category[0].sub;
+  const route = category.find((item) => item.main.label == "사업소개");
+  const subRoutes = route.sub;
 
   return (
     <section className={styles.section}>
       <div className="container">
         <h2 className="section_title">사업소개</h2>
         <ul className={styles.category__list}>
-          {routes.map((route) => (
+          {subRoutes.map((sub) => (
             <li
-              key={route.label}
+              key={sub.label}
               className={`trigger ${styles.category__item}`}
               data-aos="fade-up"
               data-aos-anchor-placement="center-bottom"
               data-aos-duration="9000"
             >
-              <h5 className={styles.category__title}>{route.label}</h5>
+              <Link to={`${sub.path}/${sub.items[0].path}`}>
+                <h5 className={styles.category__title}>{sub.label}</h5>
+              </Link>
               <ul className={styles.category__sub__list}>
-                {route.items.map((item) => (
+                {sub.items.map((item) => (
                   <li className={styles.category__sub__item} key={item.path}>
                     <Link
-                      to={`${route.path}/${item.path}`}
+                      to={`${sub.path}/${item.path}`}
                       className={styles.category__sub__link}
                     >
                       {item.label}
