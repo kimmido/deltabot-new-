c72c596189fab998c3c82981adabe5a167291fe7
kimmido <icecream120102@gmail.com>
2025-02-18 17:08:22 +0900

    floating 그림자 추가

--- src/components/Shared/ScrollTopBtn.jsx
+++ src/components/Shared/ScrollTopBtn.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useRef } from "react";
 import IconArrow from "../icons/IconArrow";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import ScrollTrigger from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(useGSAP, ScrollTrigger);
 
 function ScrollTopBtn() {
+  const btnRef = useRef(null);
+
+  // useGSAP(() => {
+  //   ScrollTrigger.create({
+  //     start: "top+=300", // 스크롤이 300px 이상 내려가면 버튼 표시
+  //     onUpdate: (self) => {
+  //       setShowButton(self.scroll().top > 300);
+  //     },
+  //   });
+  // });
+
   const onClick = () => {
     window.scrollTo({
       top: 0,
@@ -9,10 +25,11 @@ function ScrollTopBtn() {
       behavior: "smooth",
     });
   };
+
   return (
-    <div className="ScrollTopBtn">
+    <div className="ScrollTopBtn" ref={btnRef}>
       <button className={`radius-pill `} onClick={onClick}>
-        <IconArrow size="24" weight="0" />
+        <IconArrow size="24" weight="1  " />
         <span className="text">TOP</span>
 
         <span className="effect"></span>
