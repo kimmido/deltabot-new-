1839e10885df6c78da1bc47a22be658b5930f4e5
kimmido <icecream120102@gmail.com>
2025-02-19 11:47:26 +0900

    스펙을 펼친 다음 탭메뉴를 눌렀을 때 페이지 상단으로 제대로 가지 않는  오류 수정

--- src/utils/scrollToTop.js
+++ src/utils/scrollToTop.js
@@ -3,20 +3,15 @@ import { useLocation } from 'react-router-dom';
 
 const ScrollToTop = () => {
 	const { pathname } = useLocation();
-    // const [beforePath, setBeforePath] = useState(pathname.split('/')[1] || 'home');
-    
-    // function findPath(pathname) {
-    //     const firstPath = pathname.split('/')[1] || 'home';
-
-    //     if (beforePath !== firstPath) {
-    //         window.scrollTo(0, 0);
-    //         setBeforePath(firstPath);
-    //     }
-    // }
 
 	useEffect(() => {
-        // findPath(pathname);
+        const handleScrollTop = () => {
             window.scrollTo(0, 0);
+        };
+        
+        requestAnimationFrame(() => {
+            requestAnimationFrame(handleScrollTop);
+        });
 	}, [pathname]);
 	return null;
 };
