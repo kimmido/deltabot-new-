04379823024466a9377a1c785e2c1fd0e7315d57
kimmido <icecream120102@gmail.com>
2025-04-16 09:30:22 +0900

    솔루션 임시 레이아웃

--- src/layouts/SolutionLayout.jsx
+++ src/layouts/SolutionLayout.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import PageHeading from "../components/UI/PageHeading";
 
 function SolutionLayout() {
-  return <div>SolutionLayout</div>;
+  return (
+    <div className="SolutionLayout">
+      <div className="container">
+        <PageHeading title="PCR" currentPath="pcr" />
+        <img src="/images/solution/pcr.jpg" />
+      </div>
+    </div>
+  );
 }
 
 export default SolutionLayout;
