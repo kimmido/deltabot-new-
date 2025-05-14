e62983b7b1bb789021a9d44ee98b76d2d20d6ba6
kimmido <icecream120102@gmail.com>
2025-05-14 11:53:39 +0900

    오류 수정

--- src/pages/Solution/components/StepFlow.jsx
+++ src/pages/Solution/components/StepFlow.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import SvgrComponent from "../../../components/icons/svgrComponent";
+import SvgrComponent from "../../../components/icons/SvgrComponent";
 
 function StepFlow({ steps }) {
   return (
@@ -7,14 +7,14 @@ function StepFlow({ steps }) {
       <h3 className="sec__title">자동화 공정</h3>
       <div className="flow-wrapper">
         {steps.map((step, idx) => (
-          <>
+          <React.Fragment key={step.title}>
             <div className="step">
               <SvgrComponent name={step.iconName} />
               <div className="step-title">{step.title}</div>
               <div className="step-sub">{step.text}</div>
             </div>
             {idx + 1 < steps.length && <div className="arrow-right">→</div>}
-          </>
+          </React.Fragment>
         ))}
       </div>
     </section>
