10bd794ed72f95038b350973ac9c26703f90dff8
kimmido <icecream120102@gmail.com>
2025-06-17 16:26:21 +0900

    메인 - 팝업 기능 연결 해놓기

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,8 +4,8 @@ import BusinessSection from "./components/BusinessSection";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import VideoSection from "./components/VideoSection";
-import MetaTag from "../../components/Layout/MetaTag";
 import SolutionSection from "./components/SolutionSection";
+import PopupManager from "../../components/UI/PopupManager";
 gsap.registerPlugin(useGSAP);
 
 const Home = () => {
@@ -30,14 +30,8 @@ const Home = () => {
 
   return (
     <>
-      {/* <MetaTag
-        title="deltabot | 델타봇"
-        description="산업용 자동화 기기 전문 제조 및 판매 업체"
-        keywords="델타봇, 로봇, 자동화, 비전시스템, 재활용시스템"
-        imgsrc="https://icpmalldata.mycafe24.com/images/og.jpg"
-        url="https://icpmalldata.mycafe24.com/"
-      /> */}
       <div className="Home">
+        <PopupManager />
         <BannerSwiper />
         <div className="container">
           <div className="decorative-area" ref={gsapContainer}>
