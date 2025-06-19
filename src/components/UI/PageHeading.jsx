50f1e75d34410a8929b95a3515f7e111b17dde19
kimmido <icecream120102@gmail.com>
2025-06-19 17:18:25 +0900

    페이지 헤딩  영상 로드 중  이미지 애니메이션이 보이는 문제 보완

--- src/components/UI/PageHeading.jsx
+++ src/components/UI/PageHeading.jsx
@@ -7,24 +7,29 @@ function PageHeading({ title, currentPath }) {
   const backgroundRef = useRef(null);
   const titleRef = useRef(null);
   const [videoLoaded, setVideoLoaded] = useState(false);
-  const [isVideo, setVideo] = useState(true);
   const [isTitle, setTitle] = useState("");
 
+  const videoPage = [
+    "collaboration",
+    "serial-robot",
+    "deltarobot",
+    "spectral-camera",
+  ];
+
   useGSAP(() => {
-    if (!videoLoaded) {
-      gsap.fromTo(
-        backgroundRef.current,
-        {
-          scale: 1.08,
-        },
-        {
-          scale: 1,
-          duration: 6,
-          ease: "none",
-        }
-      );
-    }
-  }, [videoLoaded]);
+    if (videoPage.includes(currentPath)) return;
+    gsap.fromTo(
+      backgroundRef.current,
+      {
+        scale: 1.08,
+      },
+      {
+        scale: 1,
+        duration: 6,
+        ease: "none",
+      }
+    );
+  }, [currentPath]);
 
   const formatWord = (word) => {
     return word.replace(/\b\w/g, (char) => char.toUpperCase()); // 각 단어의 첫 글자를 대문자로 변환
@@ -40,7 +45,6 @@ function PageHeading({ title, currentPath }) {
 
   return (
     <div className="PageHeading">
-      {/* {console.log("페이지 헤딩")} */}
       <div className="background">
         <div
           ref={backgroundRef}
@@ -49,7 +53,7 @@ function PageHeading({ title, currentPath }) {
             backgroundImage: `url(/images/heading/heading_${currentPath}.jpg)`,
           }}
         ></div>
-        {isVideo && (
+        {videoPage.includes(currentPath) && (
           <video
             key={currentPath}
             muted
@@ -67,9 +71,6 @@ function PageHeading({ title, currentPath }) {
             />
           </video>
         )}
-        {/* {isVideoLoad ? (
-        ) : (
-        )} */}
       </div>
       <div className="page__text-box overflow_hidden">
         <h2
@@ -82,7 +83,6 @@ function PageHeading({ title, currentPath }) {
           {isTitle}
         </h2>
       </div>
-      {/* <p>이미지 경로: @assets/imagse/page_heading_{img}.jpg</p> */}
     </div>
   );
 }
